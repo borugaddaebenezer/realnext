867029386629478943a36a8e467a577c64ca9212
borugaddaebenezer <chantiborugadda5@gmail..com>
2025-07-02 19:27:46 +0530

    update the header and make the profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar:{
         type:String,
-        default: "https://www.bing.com/ck/a?!&&p=de694d01fa2a721ba2b7f48147488b3e740ec56bcfb85b4c608d28bdc9407d1fJmltdHM9MTc1MTQxNDQwMA&ptn=3&ver=2&hsh=4&fclid=10794f42-a218-652d-0070-5a89a36764cd&u=a1L2ltYWdlcy9zZWFyY2g_cT1wcm9maWxlK2ltYWdlJmlkPTI3MzNBNDVGRkJDMDZFRTEyODkzMTkzQ0E1M0U0NzM0RTZENDg3RTAmRk9STT1JQUNGSVI&ntb=1",
+        default: 'https://img.freepik.com/premium-photo/profile-icon-white-background_941097-161740.jpg',
     },
 }, {timestamps:true});
 
