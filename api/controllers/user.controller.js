c07e8a67ef1addeb60fd54407dff15d564ded85f
borugaddaebenezer <chantiborugadda5@gmail..com>
2025-07-20 23:50:54 +0530

    create user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,41 +1,41 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-export const test=(req,res)=>{
-    res.json({
-        message:'Hello api',
-    });
+import { errorHandler } from "../utils/error.js";
+export const test = (req, res) => {
+  res.json({
+    message: "Hello api",
+  });
 };
 
 export const updateUser = async (req, res, next) => {
-  console.log("this is the form data received",req.body)
+  console.log("this is the form data received", req.body);
   const userId = req.userId;
-  console.log('userid from diff sources are',userId,req.params.id);
-  console.log(req.body.username)
-  
+  console.log("userid from diff sources are", userId, req.params.id);
+  console.log(req.body.username);
+
   if (userId != req.params.id)
-    return next(errorHandler("You can update Your Own Account Only!", 401));
+    return next(errorHandler(401,"You can update Your Own Account Only!"));
   if (req.body.password) {
     req.body.password = bcrypt.hashSync(req.body.password, 10);
   }
   try {
     const updatedUser = await User.findByIdAndUpdate(
-    userId,
-    {
-      $set: {
-        username: req.body.username,
-        password:req.body.password,
-        email: req.body.email,
-        photo: req.body.photo,
+      userId,
+      {
+        $set: {
+          username: req.body.username,
+          password: req.body.password,
+          email: req.body.email,
+          avatar: req.body.avatar,
+        },
       },
-    },
-    { new: true }
-  );
-  console.log(updatedUser);
+      { new: true }
+    );
+    console.log(updatedUser);
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
   } catch (error) {
-    res.json(error)
+    res.json(error);
   }
-  
-  
-  const {password,...rest} = updatedUser._doc;
-  res.status(200).json(rest)
+
 };
--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -3,18 +3,18 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  
+
   if (!token) return next(errorHandler("UnAuthorized!", 401));
   try {
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (err) {
-          return next(errorHandler("UnAuthorized", 401));
-        }
-        req.userId = user.id;
-        console.log("Hey from the verify user!")
-        next();
-      });
+        return next(errorHandler("UnAuthorized", 401));
+      }
+      req.userId = user.id;
+      console.log("Hey from the verify user!");
+      next();
+    });
   } catch (error) {
-    next(errorHandler("There seems to be an issue with the token",401))
-  } 
+    next(errorHandler("There seems to be an issue with the token", 401));
+  }
 };
