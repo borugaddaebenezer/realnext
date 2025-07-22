edbdd7f7f6e234d971f865d145fd563dbdf820d8
borugaddaebenezer <chantiborugadda5@gmail..com>
2025-07-22 22:16:44 +0530

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -3,7 +3,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import cors from'cors';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
@@ -12,8 +15,14 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log(err);
 });
 
+//  const __dirname = path.resolve();
 
 const app=express();
+
+app.use(cors({
+  origin: 'http://localhost:5173', // your frontend port
+  credentials: true,
+}));
 app.use(cookieParser())
 
 app.use(express.json());
@@ -24,6 +33,13 @@ app.listen(3000, ()=>{
 
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
+app.use('/api/listing', listingRouter);
+
+// app.use(express.static(path.join(__dirname, '/client/dist')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// })
 
 
 app.use((err,req,res,next)=>{
