c444362736e165419104e5231bc9270556741a38
borugaddaebenezer <chantiborugadda5@gmail..com>
2025-08-04 22:49:35 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log(err);
 });
 
-//  const __dirname = path.resolve();
+ const __dirname = path.resolve();
 
 const app=express();
 
@@ -35,11 +35,11 @@ app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing', listingRouter);
 
-// app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-// })
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 
 app.use((err,req,res,next)=>{
